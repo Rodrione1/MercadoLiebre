897f2018b63cf89b23790fcb9ddcd0c00e60c04a
Rodrione1 <alvarezrodrigofernando@gmail.com>
2022-06-13 18:31:06 -0300

    Hola

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require ("path");
+
+
+
+const publicPath = path.resolve(__dirname,"./public");
+
+app.use(express.static(publicPath));
+
+    app.get("/",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"./views/home.html"));
+    });
+    app.get("/register",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"./views/register.html"))
+     })
+    
+
+    app.listen(3001,()=>{console.log("servidor 3001")});
